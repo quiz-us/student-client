6f026f92c09c513055cde0eacd1fec1b7c55d3bf
Joshua Ling <joshling1919@gmail.com>
2019-08-27 16:51:13 -0700

    Style multiple choice

--- src/components/decks/study/MultipleChoiceResponse.js
+++ src/components/decks/study/MultipleChoiceResponse.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import { makeStyles } from '@material-ui/styles';
 import ReadOnly from './ReadOnly';
 import shuffle from '../../../util/shuffle';
 
+const useStyles = makeStyles(theme => ({
+  multipleChoice: {
+    marginBottom: theme.spacing(2),
+    fontSize: '16px'
+  }
+}));
+
 const MultipleChoiceResponse = ({ questionOptions }) => {
+  const classes = useStyles();
   console.log('uh ok', questionOptions);
   return (
     <div>
-      <h3>Choose correct answer:</h3>
+      <h3>Choose the correct answer:</h3>
       {shuffle(questionOptions).map(answerChoice => {
         const { id, optionNode } = answerChoice;
         return (
-          <CardActionArea key={`answerchoice-${id}`}>
+          <CardActionArea
+            key={`answerchoice-${id}`}
+            className={classes.multipleChoice}
+          >
             <ReadOnly value={JSON.parse(optionNode)} />
           </CardActionArea>
         );
