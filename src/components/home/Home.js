fb05abd3d66007c481919fd96c143f45007fb332
Joshua Ling <joshling1919@gmail.com>
2019-10-18 22:41:29 -0700

    Fix undefined currentStudent bug

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,11 +8,12 @@ import { GET_CURRENT_STUDENT } from '../queries/Student';
 import GlobalLoader from '../app/GlobalLoader';
 
 export default () => {
-  const { data, loading } = useQuery(GET_CURRENT_STUDENT);
+  const { data = {}, loading } = useQuery(GET_CURRENT_STUDENT);
 
   if (loading) {
     return <GlobalLoader />;
   }
+
   const { currentStudent } = data;
 
   if (!currentStudent) {
